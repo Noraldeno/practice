0c6304bd6454be14283418e3cb96c3a32159c27c
Norald Alejo <norald.alejo@gmail.com>
2016-08-27 08:14:29 -0700

    Created version 1

--- hello.java
+++ hello.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class hello{
 	public static void main(String [] args){
+		
+		//Intializes scanner
+		Scanner input = new Scanner(System.in);
+		
+		//Prints "Hello!" and asks name of user
 		System.out.println("Hello!");
+		System.out.println("What is your name?");
+		String name = input.nextLine();
+			
+		//Prints out user name and says "Nice to meet you!"
+		System.out.printf("Hi %s!! Nice to meet you!", name);
 	}
 
 }
